da521dfddc4e641c096a4e840f1cb062e9269dde
Florian Eitel <feitel@indeedgeek.de>
2010-04-23 21:36:50 +0200

    Added output to client-test and try to close a notification

--- client-test.rb
+++ client-test.rb
@@ -6,20 +6,48 @@ bus = DBus::SessionBus.instance
 service = bus.service("org.freedesktop.Notifications")
 notify = service.object("/org/freedesktop/Notifications")
 notify.default_iface = "org.freedesktop.Notifications" 
+
+puts "---------- service xml ----------"
 puts notify.introspect
+puts
+
 iface = notify["org.freedesktop.Notifications"]
 
+puts "---------- methods ----------"
 pp iface.methods
+puts
 
-iface.on_signal(bus, "ActionInvoked") do |*params| puts params.inspect end
-iface.on_signal(bus, "NotificationClosed") do |*params| puts params.inspect end
+iface.on_signal(bus, "ActionInvoked") do |*params|
+  puts "ActionInvoked #{params.inspect}"
+end
 
+iface.on_signal(bus, "NotificationClosed") do |*params|
+  puts "NotificationClosed #{params.inspect}"
+end
+
+
+puts "---------- capabilities ----------"
 puts iface.GetCapabilities.inspect
+puts
+
+puts "---------- server informations ----------"
 puts iface.GetServerInformation.inspect
-#                 app_name,     id, icon,    summary,   body,           actions,                                      hints,                                                    timeout
-id = iface.Notify("client-test", 0, "info", "subject", "B<b>od</b>Y", ["button1", "button1", "button2", "button2"], {"urgency" => [DBus::Type::Type.new(DBus::Type::BYTE), 1 ]}, -1)
-puts id
-#iface.CloseNotification id
+puts
+
+# app_name,     id, icon,    summary,   body,           actions,                                      hints,                                                    timeout
+notification = ["client-test", 0, "info", "subject", "B<b>od</b>Y", ["button1", "button1", "button2", "button2"], {"urgency" => [DBus::Type::Type.new(DBus::Type::BYTE), 1 ]}, -1]
+sleep 1
+
+id = iface.Notify *notification
+puts "send notification id=#{id}"
+
+sleep 4
+
+id = iface.Notify *notification
+puts "send notification id=#{id}"
+sleep 1
+puts "try to close notification"
+iface.CloseNotification id.first
 
 main = DBus::Main.new  
 main << bus  
